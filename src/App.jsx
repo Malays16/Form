b3370121baf08a7435ea7419dc2cb59a6d3560bc
Malays <malays92@mail.ru>
2024-07-12 19:57:46 +0300

    Fix component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import Form from './components/Form';
+import { useState } from 'react';
+import Form from './components/Form.jsx';
 import './App.css';
 
 function App() {
+  const [hexColor, setHexColor] = useState('');
+
   return (
-    <>
-      <Form />
-    </>
-  )
+    <div id="wrapper" style={{ backgroundColor: hexColor }}>
+      <Form setHexColor={setHexColor} />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,12 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
+import PropTypes from 'prop-types';
 
-const isValidHexColor = (hexColor) => {
+const isValidHexColor = hexColor => {
   const hexColorRegex = /^#([0-9A-F]{3}){1,2}$/i;
   return hexColorRegex.test(hexColor);
 };
 
-const hexToRgb = (hex) => {
+const hexToRgb = hex => {
   let r = 0, g = 0, b = 0;
   if (hex.length === 4) {
     r = parseInt(hex[1] + hex[1], 16);
@@ -19,32 +20,27 @@ const hexToRgb = (hex) => {
   return `rgb(${r}, ${g}, ${b})`;
 };
 
-const Form = () => {
-  const [ form, setForm ] = useState({
+const Form = ({ setHexColor }) => {
+  const [form, setForm] = useState({
     hexColor: '',
     info: ''
   });
 
-  const handleChange = (evt) => {
+  const handleChange = evt => {
     const { name, value } = evt.target;
     if (value.length) {
       if (!isValidHexColor(value)) {
         setForm({ ...form, [name]: value, info: 'Ошибка!' });
+        setHexColor('#f22');
       } else {
         setForm({ ...form, [name]: value, info: hexToRgb(value) });
+        setHexColor(value);
       }
     } else {
       setForm({ ...form, [name]: value, info: '' });
+      setHexColor('');
     }
   };
-
-  useEffect(() => {
-    if (form.hexColor && !isValidHexColor(form.hexColor)) {
-      document.body.style.backgroundColor = '#f22';
-    } else {
-      document.body.style.backgroundColor = form.hexColor;
-    }
-  });
 
   return (
     <form>
@@ -52,6 +48,10 @@ const Form = () => {
       <div className="info-message">{form.info}</div>
     </form>
   );
+};
+
+Form.propTypes = {
+  setHexColor: PropTypes.func.isRequired
 };
 
 export default Form;
